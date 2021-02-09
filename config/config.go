758e15f020586b49fb78a9dcb0ee666b15c0e890
alramaboli <alramaboli@gmail.com>
2021-02-09 11:14:07 +0200

    Added Config Service

---
+++ config/config.go
@@ -0,0 +1,38 @@
+package config
+
+import (
+	"os"
+
+	"gopkg.in/yaml.v2"
+)
+
+//Config ...
+type Config struct {
+	Database struct {
+		URL     string `yaml:"url"`
+		DB      string `yaml:"db"`
+		Timeout int    `yaml:"timeout"`
+	} `yaml:"database"`
+	Server struct {
+		Port string `yaml:"port"`
+		Host string `yaml:"host"`
+	} `yaml:"server"`
+}
+
+//NewConfig ...
+func NewConfig(configFile string) (*Config, error) {
+	file, err := os.Open(configFile)
+	if err != nil {
+
+		return nil, err
+	}
+	defer file.Close()
+	cfg := &Config{}
+	yd := yaml.NewDecoder(file)
+	err = yd.Decode(cfg)
+
+	if err != nil {
+		return nil, err
+	}
+	return cfg, nil
+}
--- main.go
+++ main.go
@@ -7,16 +7,20 @@ import (
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
 	"github.com/projects/hexagonal-architecture/api"
+	"github.com/projects/hexagonal-architecture/config"
 	"github.com/projects/hexagonal-architecture/domain"
 	"github.com/projects/hexagonal-architecture/repository"
 )
 
 func main() {
 
-	mongoURL := "mongodb://localhost:27017"
-	mongodb := "product"
-	timeout := 5
-	repo, _ := repository.NewMongoRepository(mongoURL, mongodb, timeout)
+	/*
+		mongoURL := "mongodb://localhost:27017"
+		mongodb := "product"
+		timeout := 5
+	*/
+	conf, _ := config.NewConfig("./config/config.yaml")
+	repo, _ := repository.NewMongoRepository(conf.Database.URL, conf.Database.DB, conf.Database.Timeout)
 	service := domain.NewProductService(repo)
 	handler := api.NewHandler(service)
 
@@ -30,6 +34,6 @@ func main() {
 	r.Post("/products", handler.Post)
 	r.Delete("/products/{code}", handler.Delete)
 	r.Get("/products", handler.GetAll)
-	log.Fatal(http.ListenAndServe(":8082", r))
+	log.Fatal(http.ListenAndServe(conf.Server.Port, r))
 
 }
